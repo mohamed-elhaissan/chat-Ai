e8db2645adfb5f6fadd060eea56b7596da8df3f7
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-08-27 11:57:59 +0200

    uploading

--- src/componanats/search.jsx
+++
@@ -1,8 +0,0 @@
-export default function Search() {
-  return (
-    <div className="serach flex justify-between ps-0 fixed bottom-[10%] pe-10 p-2 left-1/2 transform -translate-x-1/2 rounded-full shadow-[0px_10px_30px_rgba(0,0,0,0.1)] w-[50%]">
-      <input type="text" placeholder="Search..." className="border-none outline-none cursor-pointer font-jakarta  ml-4 pl-10 "/>
-      <button className="font-jakarta py-2 px-20 bg-gray-600 text-white rounded-full">Search</button>
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily :{
-        'sans' : ['Inter', 'sans-serif'],
-        'jakarta' : ['Plus Jakarta Sans', 'sans-serif'],
-      },
-      colors:{
-        'primary' : '#4A25E1',
-        'secondary' : '#CACAFF',
-        'third':'#7B5AFF',
-        'Gray':"#718096",
-      }
-      
-    },
+    extend: {},
   },
   plugins: [],
 }
