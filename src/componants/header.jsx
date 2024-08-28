955174b59fe0693435c93a11162f30731d1a148c
mohamed <mhalwasoffice@gmail.com>
2024-08-28 20:12:39 +0100

    uploading

--- src/componants/header.jsx
+++ src/componants/header.jsx
@@ -1,47 +1,27 @@
 import { useState } from "react";
-import "./main.css";
-import profile from "../assets/profile.svg";
+import { CiDark } from "react-icons/ci";
+import { MdOutlineLightMode } from "react-icons/md";
 export default function Header() {
   const [show, setShow] = useState(false);
   return (
     <>
-      <div className="flex justify-between items-center text-lg px-10 my-2">
+      <div className="flex justify-between items-center text-lg px-10 my-5">
         <h1 className="font-bold text-main cursor-pointer">Med-GPT6.7</h1>
         <div className="flex gap-5 text-2xl items-center justify-center ">
           <span className="cursor-pointer">
-            <ion-icon name="moon-outline"></ion-icon>
+            <CiDark />
           </span>
           <span className="cursor-pointer">
-            <ion-icon name="sunny-outline"></ion-icon>
+            <MdOutlineLightMode />
           </span>
         </div>
-        <div className="flex flex-row-reverse items-center gap-5">
-          <img src={profile} alt="" className="cursor-pointer" />
-          <div
-            className="togle-menu flex gap-1 flex-col cursor-pointer relative duration-200 ease-[cubic-bezier(0.16, 1, 0.3, 1)] transition-all"
-            onClick={() => {
-              setShow(!show);
-            }}
-          >
-            <span
-              className="block w-10 h-1 rounded-lg bg-main"
-              style={{
-                width: show ? "10px" : "40px",
-              }}
-            ></span>
-            <span
-              className="block w-10 h-1 rounded-lg bg-main duration-200 ease-[cubic-bezier(0.16, 1, 0.3, 1)] transition-all"
-              style={{
-                width: show ? "30px" : "40px",
-              }}
-            ></span>
-            <span
-              className="block w-10 h-1 rounded-lg bg-main duration-200 ease-[cubic-bezier(0.16, 1, 0.3, 1)] transition-all"
-              style={{
-                width: show ? "10px" : "40px",
-              }}
-            ></span>
-          </div>
+        <div className="cursor-pointer relative ">
+          <span
+            className="block absolute -top-[100%] -left-[20%] rounded-full bg-black w-[60px] z-10 h-[60px]"
+          ></span>
+          <span className="block w-10 bg-white relative z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full"></span>
+          <span className="block w-10 bg-white relative z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full"></span>
+          <span className="block w-10 bg-white relative z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full"></span>
         </div>
       </div>
       <hr></hr>
