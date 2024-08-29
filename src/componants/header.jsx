7f7c46473cf510dd502cfe043630c1bcda6215a9
mohamed <mhalwasoffice@gmail.com>
2024-08-29 12:55:41 +0100

    darkmode

--- src/componants/header.jsx
+++ src/componants/header.jsx
@@ -2,22 +2,23 @@ import './style.scss';
 import { CiDark } from "react-icons/ci";
 import { MdOutlineLightMode } from "react-icons/md";
 export default function Header() {
+ 
   return (
     <>
       <div className="flex justify-between items-center text-lg px-10 my-1">
-        <h1 className="font-bold text-main cursor-pointer">Med-GPT6.7</h1>
+        <h1 className="font-bold text-main cursor-pointer dark:text-white">Med-GPT6.7</h1>
         <div className="flex gap-5 text-2xl items-center justify-center ">
           <span className="cursor-pointer">
-            <CiDark />
+            <CiDark className='relative dark:text-white' />
           </span>
           <span className="cursor-pointer">
-            <MdOutlineLightMode />
+            <MdOutlineLightMode className='relavtive dark:text-white'/>
           </span>
         </div>
-        <div className="parentDiv cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:scale-0 before:bg-black before:w-full before:h-full before:rounded-full relative  w-[60px] h-[60px] rounded-full before:transition-all before:duration-200 before:ease-[cubic-bezier(0.16, 1, 0.3, 1)] hover:before:scale-100"
+        <div className="parentDiv cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:scale-0 before:bg-black before:w-full before:h-full before:rounded-full relative  w-[60px] h-[60px] rounded-full before:transition-all before:duration-200 before:ease-[cubic-bezier(0.16, 1, 0.3, 1)] hover:before:scale-100 dark:before:bg-white"
        >
-          <span className="child block w-10  left-2 absolute top-5 z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full"></span>
-          <span className="child block w-10  absolute left-2 bottom-5  z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full"></span>
+          <span className="child block w-10  left-2 absolute top-5 z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full dark:bg-gray-50"></span>
+          <span className="child block w-10  absolute left-2 bottom-5  z-20 h-[3px] transition-all duration-100 ease-in mb-1 rounded-full dark:bg-gray-50"></span>
         </div>
       </div>
       <hr></hr>
--- src/componants/search.jsx
+++ src/componants/search.jsx
@@ -1,13 +1,27 @@
-export default function Search(){
+import "./style.scss";
+import {IoIosMic} from "react-icons/io";
+import {IoHappyOutline} from "react-icons/io5";
+import { IoMdSend } from "react-icons/io";
+
+
+export default function Search() {
     return (
         <>
-            <div className="fixed bottom-5 left-1/2 -translate-x-1/2 flex items-center justify-between w-1/2 rounded-lg shadow-2xl bg-white px-5 py-2">
-                <input type="text" className="w-full bg-transparent outline-none border-none" placeholder="Message to Med-Gpt..."/>
+            <div
+                className="searchBox fixed bottom-5 left-1/2 -translate-x-1/2 flex items-center justify-between w-1/2 rounded-lg  bg-white px-5 py-2  ">
+                <input
+                    type="text"
+                    className="w-full bg-transparent outline-none border-none"
+                    placeholder="Message to Med-Gpt..."
+                />
                 <div className="w-1/4 flex items-center justify-center gap-1">
-                
-                <button className="flex items-center gap-1 bg-primary text-white text-sm py-3 px-5 rounded-lg">Send<ion-icon name="send-outline" ></ion-icon></button>
+                    <IoIosMic className='rounded-full  text-3xl cursor-pointer ' />
+                    <IoHappyOutline className='rounded-full  text-3xl cursor-pointer'/>
+                    <button className="flex items-center gap-1 bg-primary text-white text-sm py-3 px-5 rounded-lg">
+                        Send <IoMdSend/>
+                    </button>
                 </div>
             </div>
         </>
     );
-}   
+}
