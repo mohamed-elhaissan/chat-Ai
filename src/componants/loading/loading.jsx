960c05f42f94e4ab41f7a74d8801ca420649ce8d
mohamed <mhalwasoffice@gmail.com>
2024-09-01 21:19:53 +0100

    loading done

--- src/componants/loading/loading.jsx
+++ src/componants/loading/loading.jsx
@@ -1,10 +1,8 @@
-import '../../componants/style.scss';
-export default function Loading(){
-    return(
-        <div className="flex absolute left-0 top-0 items-center justify-center bg-black w-full h-full">
-            <div className=".loading w-52 h-52 rounded-full border-t-2">
-
-            </div>
-        </div>
-    )
+import "../../componants/style.scss";
+export default function Loading() {
+  return (
+    <div className="loading flex absolute left-0 top-0 items-center justify-center bg-black w-full h-full">
+        <span className="loading text-white text-2xl">LOADING...</span>
+    </div>
+  );
 }
