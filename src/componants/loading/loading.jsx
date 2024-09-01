0a6d3e928ae934adf3fc6668be3be2a574b1f297
mohamed <mhalwasoffice@gmail.com>
2024-09-01 21:06:12 +0100

    updating

--- src/componants/loading/loading.jsx
+++ src/componants/loading/loading.jsx
@@ -1,4 +1,10 @@
+import '../../componants/style.scss';
 export default function Loading(){
     return(
+        <div className="flex absolute left-0 top-0 items-center justify-center bg-black w-full h-full">
+            <div className=".loading w-52 h-52 rounded-full border-t-2">
+
+            </div>
+        </div>
     )
 }
